39d9d4ee480a67805b52b5a6eb391ee6298a0d21
gkzero0-cmyk <gkzero0@gmail.com>
2026-09-15 11:37:05 +0900

    fix: show 놈삐 in applicant detail

--- justserver-up-ranking-github-ready/applicant-detail-manual-overrides.js
+++ justserver-up-ranking-github-ready/applicant-detail-manual-overrides.js
@@ -12,6 +12,7 @@
     dd0705: Object.freeze({ name: '디또띠' }),
     mat981: Object.freeze({ name: '갱소리' }),
     naranggu99: Object.freeze({ name: '사일' }),
+    jyd0808: Object.freeze({ name: '놈삐' }),
     nmoohae1205: Object.freeze({
       name: '무해_',
       message: '마크 간절하고간절하게 너무너무너무하고싶습니다!!!!!!! 즐찾수도 ㅠㅠ 너무감사드립니다 충분히 높게받으실수도이쓴데 이런 기회주셔서 너무감사드립니다!!!! 복 많이 받으세요!!!',
